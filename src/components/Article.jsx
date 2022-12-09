866c5cea25ec0f389ae73529bf31f4ab9d3d1fc7
Carl-with-a-C <carlbclifton@gmail.com>
2022-12-09 09:00:07 +0000

    added back button on single article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,8 @@
 import Comments from "./Comments";
 import upvoteIcon from "../icons/upvoteIcon.svg";
+import backArrowIcon from "../icons/backArrowIcon.svg";
 
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { getArticle } from "../utils/axiosSettings";
@@ -35,6 +37,11 @@ const Article = () => {
     <h1>Article Loading...</h1>
   ) : (
     <article className="Article">
+      <Link to="/articles">
+        <button id="backArrow-btn">
+          <img src={backArrowIcon} alt="back button"></img>
+        </button>
+      </Link>
       <h1>{currentArticle.topic}</h1>
       <h2>{currentArticle.title}</h2>
       <small>
